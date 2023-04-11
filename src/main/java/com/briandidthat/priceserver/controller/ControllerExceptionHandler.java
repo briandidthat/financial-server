a31cd35e3cc0dfc91cefdfaba9e6a86ec735f267
toogroovy <blora.dev@gmail.com>
2023-04-10 22:58:28 -0400

    multiple historical spot price requests

--- src/main/java/com/briandidthat/priceserver/controller/ControllerExceptionHandler.java
+++ src/main/java/com/briandidthat/priceserver/controller/ControllerExceptionHandler.java
@@ -19,19 +19,19 @@ import java.util.List;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Error> handleResourceNotFoundException(Exception e, WebRequest request) {
+    private ResponseEntity<Error> handleResourceNotFoundException(Exception e, WebRequest request) {
         ExceptionDetails details = new ExceptionDetails(LocalDateTime.now(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity(details, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BackendClientException.class)
-    public ResponseEntity<Error> handleInternalServerError(Exception e, WebRequest request) {
+    private ResponseEntity<Error> handleInternalServerError(Exception e, WebRequest request) {
         ExceptionDetails details = new ExceptionDetails(LocalDateTime.now(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity(details, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Error> handleConstraintValidationException(MethodArgumentNotValidException e, WebRequest request) {
+    private ResponseEntity<Error> handleConstraintValidationException(MethodArgumentNotValidException e, WebRequest request) {
         List<ExceptionDetails> violations = new ArrayList<>();
         LocalDateTime date = LocalDateTime.now();
 
