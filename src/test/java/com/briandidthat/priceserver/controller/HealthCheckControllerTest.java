363fcd056febddd53a4bc5154b122281bee93c98
briandidthat <blora.dev@gmail.com>
2023-06-14 21:14:15 -0400

    health checks

---
+++ src/test/java/com/briandidthat/priceserver/controller/HealthCheckControllerTest.java
@@ -0,0 +1,20 @@
+package com.briandidthat.priceserver.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthCheckControllerTest {
+
+    @Test
+    void setAvailable() {
+    }
+
+    @Test
+    void isAvalailable() {
+    }
+
+    @Test
+    void isReady() {
+    }
+}
