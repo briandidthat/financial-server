f9d7b7b8d639350938cdbc4f7c1e7aee020c3fe6
briandidthat <blora.dev@gmail.com>
2023-06-20 23:47:54 -0400

    health

--- src/test/java/com/briandidthat/priceserver/controller/HealthCheckControllerTest.java
+++ src/test/java/com/briandidthat/priceserver/controller/HealthCheckControllerTest.java
@@ -1,15 +1,11 @@
 package com.briandidthat.priceserver.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
