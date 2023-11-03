1891e75c7639999c0bf567464936b3932243af29
briandidthat <blora.dev@gmail.com>
2023-11-03 14:11:54 -0400

    fix tests

--- src/test/java/com/briandidthat/financialserver/controller/FredControllerTest.java
+++ src/test/java/com/briandidthat/financialserver/controller/FredControllerTest.java
@@ -1,6 +1,8 @@
 package com.briandidthat.financialserver.controller;
 
+import com.briandidthat.financialserver.domain.fred.FredSeriesId;
 import com.briandidthat.financialserver.service.FredService;
+import com.briandidthat.financialserver.util.TestingConstants;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,9 +11,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.LinkedHashMap;
 
-@WebMvcTest(FredControllerTest.class)
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FredController.class)
 class FredControllerTest {
 
     @Autowired
@@ -26,6 +35,23 @@ class FredControllerTest {
     }
 
     @Test
-    void getObservations() {
+    void getObservations() throws Exception {
+        String outputJson = mapper.writeValueAsString(TestingConstants.MORTGAGE_RATE_RESPONSE);
+
+        when(service.getObservations(FredSeriesId.AVERAGE_MORTGAGE_RATE, new LinkedHashMap<>())).thenReturn(TestingConstants.MORTGAGE_RATE_RESPONSE);
+
+        this.mockMvc.perform(get("/fred/observations/{operation}","averageMortgageRate"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputJson))
+                .andDo(print());
+    }
+
+    // 400 error
+    @Test
+    void getObservationsShouldHandleOperationNotFound() throws Exception {
+        this.mockMvc.perform(get("/fred/observations/{operation}","randomOperation"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("Invalid series id. Available operations:")))
+                .andDo(print());
     }
 }
