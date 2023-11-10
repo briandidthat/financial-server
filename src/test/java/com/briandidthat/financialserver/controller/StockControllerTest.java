8fe3947778d8429b7fbe6343a702d6f0d477598c
briandidthat <blora.dev@gmail.com>
2023-11-10 14:35:20 -0500

    finalize branch

--- src/test/java/com/briandidthat/financialserver/controller/StockControllerTest.java
+++ src/test/java/com/briandidthat/financialserver/controller/StockControllerTest.java
@@ -1,15 +1,24 @@
 package com.briandidthat.financialserver.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.briandidthat.financialserver.service.TwelveService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(StockController.class)
 class StockControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @MockBean
+    private TwelveService service;
 
     @Test
     void getStockPrice() {
