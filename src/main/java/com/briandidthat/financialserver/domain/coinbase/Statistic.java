a8d287178706f059dd5d58988cfa0d2988703c12
Brian Lora <brian@brians-mbp.mynetworksettings.com>
2023-12-27 21:06:03 -0500

    rename for clarity

--- src/main/java/com/briandidthat/financialserver/domain/coinbase/Statistic.java
+++ src/main/java/com/briandidthat/financialserver/domain/coinbase/Statistic.java
@@ -2,14 +2,14 @@ package com.briandidthat.financialserver.domain.coinbase;
 
 import java.time.LocalDate;
 
-public record Statistic(String symbol, String previousPrice, String currentPrice, String priceChange,
+public record Statistic(String symbol, String startPrice, String endPrice, String priceChange,
                         String percentChange, LocalDate startDate, LocalDate endDate, String timeDelta) {
     @Override
     public String toString() {
         return "Statistic{" +
                 "symbol='" + symbol + '\'' +
-                ", previousPrice='" + previousPrice + '\'' +
-                ", currentPrice='" + currentPrice + '\'' +
+                ", startPrice='" + startPrice + '\'' +
+                ", endPrice='" + endPrice + '\'' +
                 ", priceChange='" + priceChange + '\'' +
                 ", percentChange='" + percentChange + '\'' +
                 ", startDate=" + startDate +
