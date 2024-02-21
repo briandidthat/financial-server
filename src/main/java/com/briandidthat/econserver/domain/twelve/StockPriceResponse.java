5410a828341fdc742495c6e199332cfd642aefd6
briandidthat <brian@brians-mbp.mynetworksettings.com>
2024-02-21 13:37:08 -0500

    fix yaml

--- src/main/java/com/briandidthat/econserver/domain/twelve/StockPriceResponse.java
+++ src/main/java/com/briandidthat/econserver/domain/twelve/StockPriceResponse.java
@@ -1,9 +1,7 @@
 package com.briandidthat.econserver.domain.twelve;
 
-import com.briandidthat.econserver.domain.AssetPrice;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public final class StockPriceResponse {
